f934272bb40e4bf3144cb418dba7a72d9e2f4a0e
conelov <conelove@yandex.ru>
2023-11-01 15:39:17 +0300

    target and attach

---
+++ demo/as_attach/main.cpp
@@ -0,0 +1,30 @@
+#include <QApplication>
+
+#include <qcustomplot/qcustomplot.h>
+
+
+int main(int argc, char* argv[]) {
+  QApplication app{argc, argv};
+
+  QCustomPlot plot;
+  {
+    auto const grap = plot.addGraph();
+    {
+      auto pen = grap->pen();
+      pen.setWidth(3);
+      pen.setColor(Qt::blue);
+      grap->setPen(pen);
+    }
+    constexpr auto resolution = 100;
+    constexpr auto border     = 5 * resolution;
+    for (auto i = -static_cast<int>(border); i < border; ++i) {
+      auto const x = i / static_cast<double>(resolution);
+      grap->addData(x, x * x);
+    }
+    grap->rescaleAxes();
+  }
+  plot.resize(640, 480);
+  plot.show();
+
+  return QApplication::exec();
+}
---
+++ demo/as_target/main.cpp
@@ -0,0 +1,29 @@
+#include <QApplication>
+
+#include <qcustomplot/qcustomplot.h>
+
+
+int main(int argc, char* argv[]) {
+  QApplication app{argc, argv};
+
+  QCustomPlot plot;
+  {
+    auto const grap = plot.addGraph();
+    {
+      auto pen = grap->pen();
+      pen.setWidth(3);
+      pen.setColor(Qt::red);
+      grap->setPen(pen);
+    }
+    constexpr std::size_t resolution = 100;
+    for (std::size_t i{}; i < M_PI * resolution * 2; ++i) {
+      auto const x = i / static_cast<double>(resolution);
+      grap->addData(x, std::sin(x));
+    }
+    grap->rescaleAxes();
+  }
+  plot.resize(640, 480);
+  plot.show();
+
+  return QApplication::exec();
+}
